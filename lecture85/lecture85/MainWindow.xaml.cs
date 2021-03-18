ee9704b065c4b13056b707809551a18da118c52e
YANIN NOSUWAN (ญาณินท์ โนสุวรรณ) <80687176+liyizan@users.noreply.github.com>
2021-03-18 19:06:36 +0700

    lecture85

---
+++ lecture85/lecture85/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lecture85
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Hashtable hashtable;
+        public MainWindow()
+        {
+            InitializeComponent();
+            hashtable = new Hashtable();
+        }
+
+        private void btnadd_Click(object sender, RoutedEventArgs e)
+        {
+            //add
+            hashtable.Add(txtkey.Text,txtvalue.Text);
+        }
+
+        private void btnremove_Click(object sender, RoutedEventArgs e)
+        {
+            hashtable.Remove(txtkey);
+        }
+
+        private void btnshow_Click(object sender, RoutedEventArgs e)
+        {
+            ICollection icollection = hashtable.Keys;
+            //show all
+            foreach(string key in icollection)
+            {
+                MessageBox.Show(hashtable[key].ToString());
+            }
+        }
+    }
+}
